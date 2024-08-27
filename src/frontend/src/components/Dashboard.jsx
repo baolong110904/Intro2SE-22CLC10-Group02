2b3f5500e9f02d40b7d396f0bfc035bfcc4267aa
BaoLong <baolong110904@gmail.com>
2024-08-27 18:38:29 +0700

    Delete Duration filter and update Mycourse.jsx for better coding

--- src/frontend/src/components/Dashboard.jsx
+++ src/frontend/src/components/Dashboard.jsx
@@ -14,6 +14,8 @@ import CourseGrid from "./CourseBox.jsx"
 import courseData from '../data/courseData.json';
 import Forum from "./Forum.jsx"
 import { Link } from 'react-router-dom';
+import Mycourse from "../components/Mycourse.jsx";
+
 
 const Dashboard = () => {
   const [activeSection, setActiveSection] = useState("timetable")
@@ -65,33 +67,25 @@ const Dashboard = () => {
     setCourses(result);
   };
 
-  const handleFilterChange = (filterType, value) => {
-    setFilters(prevFilters => {
-      let updatedFilters;
-      if (filterType === 'duration') {
-        // Toggle duration filter
-        const updatedDuration = prevFilters.duration.includes(value)
-          ? prevFilters.duration.filter(item => item !== value)
-          : [...prevFilters.duration, value];
-        updatedFilters = { ...prevFilters, duration: updatedDuration };
-      } else if (filterType === 'rating') {
-        // Reset rating if the same value is selected
-        updatedFilters = {
-          ...prevFilters,
-          [filterType]: prevFilters[filterType] === value ? null : value
-        };
-      } else {
-        // For other filters, update the value or reset to default if "all" is selected
-        updatedFilters = {
-          ...prevFilters,
-          [filterType]: value === "all" ? "all" : value
-        };
-      }
-
-      // Apply filters immediately
-      applyFilters(updatedFilters);
-      return updatedFilters;
-    });
+  const handleFilterChange = (filterType, value, prevFilters) => {
+    let updatedFilters;
+    if (filterType === 'duration') {
+      const updatedDuration = prevFilters.duration.includes(value)
+        ? prevFilters.duration.filter(item => item !== value)
+        : [...prevFilters.duration, value];
+      updatedFilters = { ...prevFilters, duration: updatedDuration };
+    } else if (filterType === 'rating') {
+      updatedFilters = {
+        ...prevFilters,
+        [filterType]: prevFilters[filterType] === value ? null : value
+      };
+    } else {
+      updatedFilters = {
+        ...prevFilters,
+        [filterType]: value === "all" ? "all" : value
+      };
+    }
+    return updatedFilters;
   };
 
   const handleShowMoreClick = () => {
@@ -113,240 +107,19 @@ const Dashboard = () => {
             </ContextWrapper>
           </React.StrictMode>
         )
-      case "teachingSessions":
+        case "teachingSessions":
         return (
-          <div className="course-section p-8 bg-gray-50">
-            {/* Title Section */}
-            <h2 className="text-3xl font-bold text-gray-800 mb-6">
-              My Courses
-            </h2>
-
-            {/* Teaching Sessions and Filters/Sorting Section */}
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
-              {/* Filters and Sorting Section (1/4 of the width on large screens) */}
-              <div className="lg:col-span-1 flex flex-col space-y-6">
-                {/* Filter Section */}
-                <div className="border border-gray-300 rounded-lg p-4 shadow-sm">
-                  <div className="flex items-center justify-between">
-                    <span className="text-gray-800 font-medium flex items-center">
-                      <IoFilterOutline className="mr-2 text-blue-600" /> Filter
-                    </span>
-                    <span className="text-gray-800 font-medium flex items-center">
-                      <FaSortAmountDown className="fas fa-filter mr-2 text-blue-600" />
-                      Sort by
-                    </span>
-                  </div>
-                  <div className="mt-4 space-y-4">
-                    <h3 className="text-lg font-semibold text-gray-800">Ratings</h3>
-                    <div className="space-y-2">
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="radio" name="rating" value="4.0" onChange={(e) => handleFilterChange("rating", parseFloat(e.target.value))} />
-                        <span className="text-yellow-500">★★★★☆</span>
-                        <span className="text-gray-600">4.0 & up</span>
-                        <span className="text-gray-400 ml-auto">(4,472)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="radio" name="rating" value="3.0" onChange={(e) => handleFilterChange("rating", parseFloat(e.target.value))} />
-                        <span className="text-yellow-500">★★★☆☆</span>
-                        <span className="text-gray-600">3.0 & up</span>
-                        <span className="text-gray-400 ml-auto">(3,347)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="radio" name="rating" value="2.0" onChange={(e) => handleFilterChange("rating", parseFloat(e.target.value))} />
-                        <span className="text-yellow-500">★★☆☆☆</span>
-                        <span className="text-gray-600">2.0 & up</span>
-                        <span className="text-gray-400 ml-auto">(2,214)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="radio" name="rating" value="1.0" onChange={(e) => handleFilterChange("rating", parseFloat(e.target.value))} />
-                        <span className="text-yellow-500">★☆☆☆☆</span>
-                        <span className="text-gray-600">1.0 & up</span>
-                        <span className="text-gray-400 ml-auto">(115)</span>
-                      </label>
-                    </div>
-                  </div>
-
-                  {/* Duration Filter */}
-                  <div className="mt-6">
-                    <h3 className="text-lg font-semibold text-gray-800">Duration</h3>
-                    <div className="space-y-2">
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="checkbox" name="duration" value="3-4-days" onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])} />
-                        <span className="text-gray-600">3-4 days</span>
-                        <span className="text-gray-400 ml-auto">(1,234)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="checkbox" name="duration" value="6-7-days" onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])} />
-                        <span className="text-gray-600">6-7 days</span>
-                        <span className="text-gray-400 ml-auto">(2,345)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="checkbox" name="duration" value="1-2-weeks" onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])} />
-                        <span className="text-gray-600">1-2 weeks</span>
-                        <span className="text-gray-400 ml-auto">(3,456)</span>
-                      </label>
-                      <label className="flex items-center space-x-2 cursor-pointer">
-                        <input type="checkbox" name="duration" value="3-4-weeks" onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])} />
-                        <span className="text-gray-600">3-4 weeks</span>
-                        <span className="text-gray-400 ml-auto">(4,567)</span>
-                      </label>
-                      {showMore && (
-                        <div ref={showMoreRef}>
-                          <label className="flex items-center space-x-2 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              name="duration"
-                              value="5-6-weeks"
-                              onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])}
-                            />
-                            <span className="text-gray-600">5-6 weeks</span>
-                            <span className="text-gray-400 ml-auto">(4,567)</span>
-                          </label>
-                          <label className="flex items-center space-x-2 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              name="duration"
-                              value="7-8-weeks"
-                              onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])}
-                            />
-                            <span className="text-gray-600">7-8 weeks</span>
-                            <span className="text-gray-400 ml-auto">(234)</span>
-                          </label>
-                          <label className="flex items-center space-x-2 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              name="duration"
-                              value="9-10-weeks"
-                              onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])}
-                            />
-                            <span className="text-gray-600">9-10 weeks</span>
-                            <span className="text-gray-400 ml-auto">(1,213)</span>
-                          </label>
-                          <label className="flex items-center space-x-2 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              name="duration"
-                              value="11-12-weeks"
-                              onChange={(e) => handleFilterChange("duration", [...filters.duration, e.target.value])}
-                            />
-                            <span className="text-gray-600">11-12 weeks</span>
-                            <span className="text-gray-400 ml-auto">(4,421)</span>
-                          </label>
-                        </div>
-                      )}
-                      <span
-                        className="text-blue-600 cursor-pointer mt-2 block"
-                        onClick={handleShowMoreClick}
-                      >
-                        {showMore ? "Show less ▲" : "Show more ▼"}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Topic, Subcategory, Language Filters */}
-                <div className="border border-gray-300 rounded-lg p-4 shadow-sm space-y-4">
-                  <div>
-                    <h3 className="text-lg font-semibold text-gray-800">Topic</h3>
-                    <select
-                      className="w-full bg-gray-50 border border-gray-300 rounded-lg py-2 px-3 mt-2"
-                      onChange={(e) => handleFilterChange("topic", e.target.value)}
-                    >
-                      <option value="all">All Topics</option>
-                      <option value="Language">Language</option>
-                      {/* Add more options based on your course categories */}
-                    </select>
-                  </div>
-
-                  {/* Subcategory Filter */}
-                  <div>
-                    <h3 className="text-lg font-semibold text-gray-800">Subcategory</h3>
-                    <select
-                      className="w-full bg-gray-50 border border-gray-300 rounded-lg py-2 px-3 mt-2"
-                      onChange={(e) => handleFilterChange("subcategory", e.target.value)}
-                    >
-                      <option value="all">All Subcategories</option>
-                      <option value="Pronunciation">Pronunciation</option>
-                      <option value="Words">Words</option>
-                      <option value="Composition">Composition</option>
-                      {/* Add more options based on your course subcategories */}
-                    </select>
-                  </div>
-
-                  <div>
-                    <h3 className="text-lg font-semibold text-gray-800">Language</h3>
-                    <select
-                      className="w-full bg-gray-50 border border-gray-300 rounded-lg py-2 px-3 mt-2"
-                      onChange={(e) => handleFilterChange("language", e.target.value)}
-                    >
-                      <option value="all">All Languages</option>
-                      <option value="english">English</option>
-                      <option value="spanish">Spanish</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div className="lg:col-span-3 grid grid-cols-1 md:grid-cols-3 gap-6">
-                {courses.length > 0 ? (
-                  courses.map(course => (
-                    <div
-                      key={course.id}
-                      className="bg-white shadow-lg rounded-lg p-4 overflow-hidden space-y-4 transition-transform transform hover:scale-105"
-                    >
-                      <div className="relative">
-                        <img
-                          src={course.image}
-                          alt={course.title}
-                          className="w-full h-48 object-cover"
-                        />
-                        <div className="absolute bottom-2 right-2 bg-white bg-opacity-80 rounded-full p-2">
-                          <span className="text-gray-700 text-sm">{course.category}</span>
-                        </div>
-                      </div>
-                      <div className="p-4">
-                        <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                          {course.title}
-                        </h3>
-                        {course.teachers.map((teacher, index) => (
-                          <p key={index} className="text-gray-600 mb-4">
-                            Teacher: {teacher}
-                          </p>
-                        ))}
-                        <Link to={`/course/${course.id}`}>
-                          <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 rounded-lg text-xs font-medium">
-                            Go to course
-                          </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ))
-                ) : (
-                  <div className="col-span-full text-center py-8">
-                    <p className="text-xl text-gray-600">No courses available with the selected filters.</p>
-                    <button
-                      className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
-                      onClick={() => {
-                        setFilters({
-                          rating: null,
-                          duration: [],
-                          topic: "all",
-                          subcategory: "all",
-                          language: "all"
-                        });
-                        setCourses(courseData);
-                      }}
-                    >
-                      Reset Filters
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
+          <div className="p-8 bg-gray-50">
+            <React.StrictMode>
+            <Mycourse 
+              initialCourses={courseData} 
+              handleFilterChange={handleFilterChange}
+              showMore={showMore}
+              handleShowMoreClick={handleShowMoreClick}
+            />
+            </React.StrictMode>
           </div>
         );
-
-
       case "materials":
         return (
           <div className="course-section p-8 bg-gray-50">
